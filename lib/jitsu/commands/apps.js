nodejitsu/jitsu

commit 70874760a2fa39fb4ffa6521c24e570553c63105

[bin doc refactor] Lazily exit the process when there is no more stdout. Minor refactor to the order-of-operations for `apps deploy` to solve a couple edge cases. Improve code docs and organization

lib/jitsu/commands/apps.js
@@ -7,6 +7,7 @@
  
 var eyes = require('eyes'),
     winston = require('winston'),
+    analyzer = require('require-analyzer'),
     jitsu = require('jitsu');
 
 var apps = exports;
@@ -30,64 +31,104 @@ apps.usage = [
   'from the current directory.'
 ];
 
+//
+// ### function deploy (callback) 
+// #### @callback {function} Continuation to respond to when complete.
+// Deploys an application through the following steps:
+// 1. Creates the application (if necessary)
+// 2. Creates or validates the package.json
+// 3. Packages and creates a new snapshot
+// 4. Stops the application (if neccessary)
+// 5. Starts the application
+//
 apps.deploy = function (callback) {
-  jitsu.package.get(process.cwd(), function (err, pkg) {
+  var dir = process.cwd(), pkg;
+  
+  function startApp (err, existing) {
+    if (err) {
+      winston.error('Error creating snapshot for app ' + pkg.name.magenta);
+      winston.error(err.message);
+      return callback(new Error(), true);
+    }
+    
+    if (existing.state === 'started') {
+      apps.stop(existing.name, function (err) {
+        return err ? callback(err) : apps.start(existing.name, callback);
+      });
+    }
+    else {
+      apps.start(existing.name, callback);
+    }
+  }
+  
+  function updateSnapshot (err, existing) {
+    if (err) {
+      winston.error('Error creating app ' + pkg.name.magenta + ': ' + err.message.red);
+      return callback(new Error(), true);
+    }
+    
+    jitsu.package.updateTarball(null, pkg, existing, function (err, snapshot) {
+      if (err) {
+        return callback(err, snapshot);
+      }
+      
+      winston.info('Updating application ' + existing.name.magenta);
+      jitsu.apps.update(existing.name, pkg, function (err) {
+        if (err) {
+          return callback(err);
+        }
+        
+        winston.info('Activating snapshot ' + snapshot.grey + ' for ' + existing.name.magenta);
+        jitsu.snapshots.activate(existing.name, snapshot, function (err) {
+          winston.silly('Done activating snapshot ' + snapshot.grey);
+          startApp(err, existing);
+        });
+      });
+    });
+  }
+  
+  function checkApp (err, local, before) {
     if (err) {
       winston.error(err);
       return callback(new Error(), true);
     }
     
-    function checkStarted (err, existing) {
+    pkg = local;
+    winston.silly('Checking if application ' + local.name.magenta + ' exists.');
+    jitsu.apps.view(local.name, function (err, app) {
       if (err) {
-        winston.error('Error creating snapshot for app ' + pkg.name.magenta);
-        winston.error(err.message);
-        return callback(new Error(), true);
-      }
-      
-      if (existing.state === 'started') {
-        apps.stop(existing.name, function (err) {
-          return err ? callback(err) : apps.start(existing.name, callback);
-        });
-      }
-      else {
-        apps.start(existing.name, callback);
-      }
-    }
-    
-    function checkSnapshots (err, existing) {
-      if (err) {
-        winston.error('Error creating app ' + pkg.name.magenta + ': ' + err.message.red);
-        return callback(new Error(), true);
-      }
-      
-      jitsu.package.updateTarball(null, pkg, existing, function (err, snapshot) {
-        if (err) {
-          return callback(err, snapshot);
-        }
+        winston.warn('No application exists for ' + local.name.magenta);
         
-        winston.info('Updating application ' + existing.name.magenta);
-        jitsu.apps.update(existing.name, pkg, function (err) {
+        //
+        // If there is a before filter we should run before creating the app do so, 
+        // then create the application.
+        //
+        return !before ? apps.create(local, updateSnapshot) : before(app, function (err, updated) {
           if (err) {
             return callback(err);
           }
           
-          winston.info('Activating snapshot ' + snapshot.grey + ' for ' + existing.name.magenta);
-          jitsu.snapshots.activate(existing.name, snapshot, function (err) {
-            winston.silly('Done activating snapshot ' + snapshot.grey);
-            checkStarted(err, existing);
-          });
+          pkg = updated;
+          apps.create(updated, updateSnapshot);
         });
-      });
-    }
-    
-    winston.silly('Checking if application ' + pkg.name.magenta + ' exists.');
-    jitsu.apps.view(pkg.name, function (err, app) {
-      if (err) {
-        winston.warn('No application exists for ' + pkg.name.magenta);
-        return apps.create(pkg.name, checkSnapshots);
       }
       
-      checkSnapshots(null, app);
+      return !before ? updateSnapshot(null, app) : before(app, function (err, updated) {
+        pkg = updated;
+        return err ? updateSnapshot(err) : updateSnapshot(null, updated);
+      })
+    });
+  }
+  
+  jitsu.package.read(dir, function (err, local) {
+    if (err) {
+      return jitsu.package.get(dir, checkApp);
+    }
+
+    checkApp(null, local, function (app, done) {
+      app = jitsu.utils.formatApp(app, ['snapshots', 'active', 'user', 'state'])
+      var pkg = analyzer.merge({}, local, app);
+      jitsu.package.validate(pkg, dir, done);  
     });
   });
 };
@@ -102,6 +143,8 @@ apps.deploy.usage = [
   '  4. Stops the application (if neccessary)',
   '  5. Starts the application',
   '',
+  'jitsu deploy',
+  'jitsu apps deploy'
 ];
 
 //
@@ -220,10 +263,8 @@ apps.view = function (name, callback) {
         return callback(err);
       }
       
-      //
-      // TODO (indexzero): Better object inspection
-      //
-      eyes.inspect(app);
+      app = jitsu.utils.formatApp(app);
+      jitsu.log.putObject(app);
       callback(null, app);
     });
   }

bin/jitsu
@@ -10,4 +10,8 @@ var eyes = require('eyes'),
 
 jitsu.start(argv._, function () {
   winston.info('Nodejitsu'.grey + ' ok'.green.bold);
+  
+  process.stdout.on('drain', function () {
+    process.exit(0);
+  })
 });

lib/jitsu/package.js
@@ -18,6 +18,25 @@ var util = require('util'),
 var package = exports;
 
 //
+// ### function get (dir, callback)
+// #### @dir {string} Directory to get the package.json from
+// #### @callback {function} Continuation to respond to when complete
+// Attempts to read the package.json from the specified `dir`. If it is
+// unable to do, walks the user through creating a new one from scratch.
+//
+package.get = function (dir, callback) {
+  package.read(dir, function (err, pkg) {
+    if (err) {
+      return package.create(dir, callback);
+    }
+    
+    package.validate(pkg, dir, function (err, updated) {
+      return err ? callback(err) : callback(null, updated);
+    });
+  });
+};
+
+//
 // ### function read (dir, callback)
 // #### @dir {string} Directory to read the package.json from
 // #### @callback {function} Continuation to pass control to when complete
@@ -105,61 +124,13 @@ package.create = function (dir, callback) {
 };
 
 //
-// ### function writePackage (pkg, dir, callback) 
-// #### @pkg {Object} Data for the package.json
-// #### @dir {string} Directory to write the package.json in
+// ### function validate (pkg, dir, callback)
+// #### @pkg {Object} Parsed package.json to validate
+// #### @dir {string} Directory containing the package.json file
 // #### @callback {function} Continuation to respond to when complete
-// Prompts the user about writing the new package.json file. If the user
-// OKs the operation, attempts to write to file. Otherwise restarts the
-// create operation in the specified `dir`.
+// Validates the specified `pkg` against the properties list 
+// returned from `package.properties(dir)`.
 //
-package.write = function (pkg, dir, create, callback) {
-  if (!callback) {
-    callback = create;
-    create = null;
-  }
-  
-  winston.warn('About to write ' + path.join(dir, 'package.json').magenta);
-  jitsu.log.putObject(pkg, 2);
-  jitsu.prompt.get({
-    name: 'answer',
-    message: 'Is this ' + 'ok?'.green.bold,
-    default: 'yes'
-  }, function (err, result) {
-    if (result.answer !== 'yes' && result.answer !== 'y') {
-      return create ? package.create(dir, callback) : callback(new Error('Save package.json cancelled.'));
-    }
-    
-    fs.writeFile(path.join(dir, 'package.json'), JSON.stringify(pkg, null, 2), function (err) {
-      return err ? callback(err) : callback(null, pkg, dir);
-    })
-  });
-};
-
-package.getVersion = function (version) {
-  var match = semver.expressions.parse(version),
-      major = match[1],
-      minor = match[2],
-      patch = match[3],
-      build = match[4];
-  
-  var compile = [major, minor, patch];
-  
-  if (build) {
-    //
-    // If there's a build string, extrapolate any numbers from it,
-    // and increment them, then push it back to the string to build.
-    //
-    build = build.match(/^-?(\d{1,3})-?$/)[1] || 0;
-    build = parseInt(build, 10) + 1;
-  }
-  else {
-    build = 1;
-  }
-  
-  return compile.join('.') + '-' + build;
-};
-
 package.validate = function (pkg, dir, callback) {
   var properties = package.properties(dir),
       missing = [];
@@ -201,114 +172,83 @@ package.validate = function (pkg, dir, callback) {
       });
       
       return jitsu.prompt.addProperties(pkg, missing, function (err, updated) {
-        return err ? callback(err) : package.checkDependencies(updated, dir, function (err, addedDeps) {
+        return err ? callback(err) : package.analyzeDependencies(updated, dir, function (err, addedDeps) {
           return err ? callback(err) : package.write(addedDeps, dir, true, callback);
         })
       });
     }
     
-    package.checkDependencies(pkg, dir, function (err, updated) {
+    package.analyzeDependencies(pkg, dir, function (err, updated) {
       return err ? callback(err) : callback(null, updated);
     });
   });
 };
 
-package.checkDependencies = function (pkg, dir, callback) {
+//
+// ### function writePackage (pkg, dir, callback) 
+// #### @pkg {Object} Data for the package.json
+// #### @dir {string} Directory to write the package.json in
+// #### @callback {function} Continuation to respond to when complete
+// Prompts the user about writing the new package.json file. If the user
+// OKs the operation, attempts to write to file. Otherwise restarts the
+// create operation in the specified `dir`.
+//
+package.write = function (pkg, dir, create, callback) {
+  if (!callback) {
+    callback = create;
+    create = null;
+  }
+  
+  winston.warn('About to write ' + path.join(dir, 'package.json').magenta);
+  jitsu.log.putObject(pkg, 2);
+  jitsu.prompt.get({
+    name: 'answer',
+    message: 'Is this ' + 'ok?'.green.bold,
+    default: 'yes'
+  }, function (err, result) {
+    if (result.answer !== 'yes' && result.answer !== 'y') {
+      return create ? package.create(dir, callback) : callback(new Error('Save package.json cancelled.'));
+    }
+    
+    fs.writeFile(path.join(dir, 'package.json'), JSON.stringify(pkg, null, 2), function (err) {
+      return err ? callback(err) : callback(null, pkg, dir);
+    })
+  });
+};
+
+//
+// ### function checkDependencies (pkg, dir, callback) 
+// #### @pkg {Object} Parsed package.json to check dependencies for.
+// #### @dir {string} Directory containing the package.json file.
+// #### @callback {function} Continuation to respond to when complete.
+// Analyzes the dependencies in `pkg` using the `require-analyzer` module.
+//
+package.analyzeDependencies = function (pkg, dir, callback) {
   winston.info('Analyzing your application dependencies in ' + pkg.scripts.start.magenta);
   analyzer.analyze({ target: path.join(process.cwd(), pkg.scripts.start), timeout: 1000 }, function (err, pkgs) {
     //
     // Create a hash of `'package': '>= version'` for the new dependencies
     //
-    var updated = analyzer.extractVersions(pkgs);
+    var updates, versions = analyzer.extractVersions(pkgs);
     
-    if (package.newDependencies(pkg.dependencies, updated)) {
+    if (package.newDependencies(pkg.dependencies, versions)) {
       winston.info('Found new dependencies');
-      jitsu.log.putObject(updated);
+      
+      //
+      // Extract, merge, and display the updates found by `require-analyzer`
+      //
+      updates = analyzer.updates(pkg.dependencies, versions);
+      updates = analyzer.merge({}, updates.added, updates.updated);
+      jitsu.log.putObject(updates);
 
       // 
       // Update the package.json dependencies
       //
-      pkg.dependencies = analyzer.merge({}, pkg.dependencies || {}, updated);
+      pkg.dependencies = analyzer.merge({}, pkg.dependencies || {}, updates);
     }
     
     callback(null, pkg);
   });
-};
-
-//
-// ### function getPackage (dir, callback)
-// #### @dir {string} Directory to get the package.json from
-// #### @callback {function} Continuation to respond to when complete
-// Attempts to read the package.json from the specified `dir`. If it is
-// unable to do, walks the user through creating a new one from scratch.
-//
-package.get = function (dir, callback) {
-  package.read(dir, function (err, pkg) {
-    if (err) {
-      return package.create(dir, callback);
-    }
-    
-    package.validate(pkg, dir, function (err, updated) {
-      return err ? callback(err) : callback(null, updated);
-    });
-  });
-};
-
-package.updateTarball = function (version, pkg, existing, callback) {
-  jitsu.package.createTarball(process.cwd(), function (err, ign, filename) {
-    if (err) {
-      return callback(err, true)
-    }
-    
-    function executeCreate (err) {
-      if (err) {
-        return callback(err, true);
-      }
-      
-      version = version || pkg.version;
-      winston.warn('Creating new snapshot for version ' + pkg.version.magenta);
-      winston.silly('Filename: ' + filename);
-      jitsu.snapshots.create(pkg.name, version, filename, function (err, snapshots) {
-        winston.info('Done creating snapshot ' + version.magenta);
-        return err ? callback(err) : callback(null, version, pkg);
-      });
-    }
-    
-    winston.silly('Existing version: ' + existing.version.magenta);
-    winston.silly('Local version: ' + pkg.version.magenta);
-    var old = semver.gte(existing.version, pkg.version),
-        update = package.newDependencies(existing.dependencies, pkg.dependencies);
-    
-    if (old) {
-      //
-      // If the existing version is greater than the version in the
-      // package.json file on disk, update it and then write back to disk.
-      //
-      winston.warn('Your package.json version will be incremented for you automatically.');
-      pkg.version = package.getVersion(existing.version);
-    }
-    
-    if (update) {
-      //
-      // If there are new dependencies, indicate this to the user.
-      //
-      winston.warn('New dependencies will be added for you automatically.');
-    }
-    
-    if (old || update) {
-      return package.write(pkg, process.cwd(), executeCreate);
-    }
-    
-    //
-    // Otherwise, simply execute the creation of the snapshot as normal.
-    //
-    executeCreate();
-  });
-};
-
-package.newDependencies = function (current, updated) {
-  var updates = analyzer.updates(current, updated);
-  return Object.keys(updates.added).length > 0 || Object.keys(updates.updated).length > 0;
 };
 
 //
@@ -317,7 +257,12 @@ package.newDependencies = function (current, updated) {
 // #### @callback {function} Continuation to pass control to when complete
 // Creates a *.tgz package file from the specified directory `dir`.
 //
-package.createTarball = function (dir, callback) {
+package.createTarball = function (dir, version, callback) {
+  if (!callback) {
+    callback = version;
+    version = null;
+  }
+  
   package.read(dir, function (err, pkg) {
     if (err) {
       return callback(err);
@@ -329,7 +274,7 @@ package.createTarball = function (dir, callback) {
     
     var parent = path.dirname(dir),
         base = path.basename(dir),
-        tarball = path.join(jitsu.config.settings.tmproot, [jitsu.config.username, pkg.name, pkg.version].join('-') + '.tgz'),
+        tarball = path.join(jitsu.config.settings.tmproot, [jitsu.config.username, pkg.name, version || pkg.version].join('-') + '.tgz'),
         target = fs.createWriteStream(tarball),
         tarargs = ['-cvf', '-', '-C', parent, base],
         tar, gzip, errState;
@@ -359,6 +304,112 @@ package.createTarball = function (dir, callback) {
   });
 };
 
+//
+// ### function updateTarball (version, pkg, existing, callback)
+// #### @version {string} **Optional** Version to use for the updated tarball
+// #### @pkg {Object} Current package.json file on disk
+// #### @existing {Object} Remote package.json stored at Nodejitsu
+// #### @callback {function} Continuation to respond to when complete.
+// 
+//
+package.updateTarball = function (version, pkg, existing, callback) {
+  function executeCreate (err) {
+    if (err) {
+      return callback(err, true);
+    }
+    
+    version = version || pkg.version;
+    jitsu.package.createTarball(process.cwd(), version, function (err, ign, filename) {
+      if (err) {
+        return callback(err, true)
+      }
+
+      winston.warn('Creating new snapshot for version ' + pkg.version.magenta);
+      winston.silly('Filename: ' + filename);
+      jitsu.snapshots.create(pkg.name, version, filename, function (err, snapshots) {
+        winston.info('Done creating snapshot ' + version.magenta);
+        return err ? callback(err) : callback(null, version, pkg);
+      });
+    });
+  }
+  
+  winston.silly('Existing version: ' + existing.version.magenta);
+  winston.silly('Local version: ' + pkg.version.magenta);
+  var old = semver.gte(existing.version, pkg.version),
+      update = package.newDependencies(existing.dependencies, pkg.dependencies);
+  
+  if (old) {
+    //
+    // If the existing version is greater than the version in the
+    // package.json file on disk, update it and then write back to disk.
+    //
+    winston.warn('Your package.json version will be incremented for you automatically.');
+    pkg.version = package.bumpVersion(existing.version);
+  }
+  
+  if (update) {
+    //
+    // If there are new dependencies, indicate this to the user.
+    //
+    winston.warn('New dependencies will be added for you automatically.');
+  }
+  
+  return old || update 
+    ? package.write(pkg, process.cwd(), executeCreate)
+    : executeCreate();
+};
+
+//
+// ### function bumpVersion (version)
+// #### @version {string} Existing version to bump
+// Returns an incremental copy of `version` with the
+// build number updated. e.g:
+//
+//     package.bumpVersion('0.1.1-2');
+//     // '0.1.1-3'
+//
+package.bumpVersion = function (version) {
+  var match = semver.expressions.parse(version),
+      major = match[1],
+      minor = match[2],
+      patch = match[3],
+      build = match[4];
+  
+  var compile = [major, minor, patch];
+  
+  if (build) {
+    //
+    // If there's a build string, extrapolate any numbers from it,
+    // and increment them, then push it back to the string to build.
+    //
+    build = build.match(/^-?(\d{1,3})-?$/)[1] || 0;
+    build = parseInt(build, 10) + 1;
+  }
+  else {
+    build = 1;
+  }
+  
+  return compile.join('.') + '-' + build;
+};
+
+//
+// ### function newDependencies (current, updated)
+// #### @current {Object} Set of current dependencies
+// #### @updated {Object} Set of updated dependencies
+// Returns a value indicating if there are any new dependencies
+// in `updated` as compared to `current`.
+//
+package.newDependencies = function (current, updated) {
+  var updates = analyzer.updates(current, updated);
+  return Object.keys(updates.added).length > 0 || Object.keys(updates.updated).length > 0;
+};
+
+//
+// ### function properties (dir)
+// #### @dir {string} Directory in which the package.json properties are being used
+// Returns a new set of properties to be consumed by `jitsu.prompt` to walk a user 
+// through creating a new `package.json` file.
+//
 package.properties = function (dir) {
   return [
     {

lib/jitsu/utils/index.js
@@ -33,6 +33,35 @@ utils.formatTime = function (obj) {
 };
 
 //
+// ### function formatApp (app, exclude)  
+// #### @app {Object} Application document to format
+// #### @exclude {Array} **Optional** List of additional properties to exclude
+// Formats the specified `app` by removing unnecessary properties combined with
+// any additional properties the caller wishes to `exclude`.
+//
+utils.formatApp = function (app, exclude) {
+  var snapshots = app.snapshots,
+      remove = ['_id', '_rev', 'drones', 'id', 'maxDrones', 'resource'];
+  
+  app.snapshots = !snapshots ? [] : snapshots.map(function (snap) {
+    return {
+      id: snap.id,
+      created: jitsu.utils.formatTime(new Date(snap.ctime))
+    }
+  });
+  
+  if (exclude) {
+    remove = remove.concat(exclude);
+  }
+  
+  remove.forEach(function (key) {
+    delete app[key];
+  });
+  
+  return app;
+}
+
+//
 // ### function missingKeys (source, target) 
 // #### @source {Array} List of keys for the current object
 // #### @target {Array} List of keys for the new object

commit e68f368e6f0d018e9365f08fc892480858716224

[fix] fix issue #40. some errors where introduced in 70874760a2fa39fb4ffa . existing.state is deleted, so the branch which stops the app only when exiting.state === 'started' never gets executed.

lib/jitsu/commands/apps.js
@@ -50,15 +50,17 @@ apps.deploy = function (callback) {
       winston.error(err.message);
       return callback(new Error(), true);
     }
-    
-    if (existing.state === 'started') {
+
+//    if (existing.state === 'started') {
+//existing.state is thrown away in check app. 
+//these closures would need to be rearanged so that this closure can see the result of view..
       apps.stop(existing.name, function (err) {
         return err ? callback(err) : apps.start(existing.name, callback);
       });
-    }
-    else {
-      apps.start(existing.name, callback);
-    }
+//    }
+//    else {
+//      apps.start(existing.name, callback);
+//    }
   }
   
   function activateSnapshot (existing, snapshot) {
@@ -84,7 +86,7 @@ apps.deploy = function (callback) {
     if (err) {
       return callback(err, true, true);
     }
-    
+
     jitsu.package.updateTarball(null, pkg, existing, firstSnapshot, function (err, snapshot) {
       if (err) {
         return callback(err, snapshot);
@@ -105,7 +107,6 @@ apps.deploy = function (callback) {
     jitsu.apps.view(local.name, function (err, app) {
       if (err) {
         winston.warn('No application exists for ' + local.name.magenta);
-        
         //
         // If there is a before filter we should run before creating the app do so, 
         // then create the application.