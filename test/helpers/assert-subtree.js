77e56bf6aa7a1ae6a938bec4358e8e32c6f78a41
indexzero <charlie.robbins@gmail.com>
2011-06-24 23:20:59 -0400

    [minor] Style updates for assert-subtree-test.js

--- test/helpers/assert-subtree.js
+++ test/helpers/assert-subtree.js
@@ -1,35 +1,52 @@
-
+/*
+ * assert-subtree.js: Custom deep object checking
+ *
+ * (C) 2010, Nodejitsu Inc.
+ *
+ */
+ 
 var assert = require('assert')
 
-function deepObjectCheck(actual, expected) {
+function deepObjectCheck (actual, expected) {
+  if (expected && actual && (expected.length != actual.length)
+    || typeof actual != typeof expected) {
+    return false;
+  }
 
-  if(expected && actual && (expected.length != actual.length)) return false;
-  if(typeof actual != typeof expected) return false;
-
-  for(var key in expected) {
-
-    var actualType = typeof(actual[key]);
-    var expectedType = typeof(expected[key]);
+  for (var key in expected) {
+    var actualType = typeof(actual[key]),
+        expectedType = typeof(expected[key]);
   
-    if(actualType != expectedType) return false;
-    if(actualType == "function") {
+    if (actualType != expectedType) {
+      return false;
+    }
+    
+    if (actualType == "function") {
       continue;
-    } else if(actualType == "object") {
-      if(!deepObjectCheck(expected[key]	,actual[key])) return false;
-    } else {
-      if(actual[key] !== expected[key]) return false;
+    } 
+    else if (actualType == "object") {
+      if (!deepObjectCheck(expected[key], actual[key])) {
+        return false;
+      }
+    } 
+    else if (actual[key] !== expected[key]) {
+      return false;
     }
   }
  
   return true;
 };
 
-exports.subtree = subtree
 
 function subtree (actual, expected, message) {
   if (!deepObjectCheck(actual, expected)) {
-//    throw new (assert.AssertionError)({actual: actual, expected: expected, opperator: 'subtree'})
-      //getting weird error if I use the propper assert.fail()
-      throw new Error(JSON.stringify({actual: actual, expected: expected, opperator: 'subtree'}))
+    //
+    // Remark: getting weird error if I use the propper `assert.fail()`.
+    //
+    // throw new (assert.AssertionError)({actual: actual, expected: expected, opperator: 'subtree'})
+    //
+    throw new Error(JSON.stringify({actual: actual, expected: expected, opperator: 'subtree'}))
   }
 }
+
+exports.subtree = subtree;
