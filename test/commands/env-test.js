15a6176d4261174fb97c94a618de15be5c48d3c5
indexzero <charlie.robbins@gmail.com>
2011-08-14 16:48:29 -0400

    [minor api test] Added `jitsu env *` commands for manipulating application environment variables

---
+++ test/commands/env-test.js
@@ -0,0 +1,91 @@
+/*
+ * apps.js: Tests for `jitsu apps *` command(s).
+ *
+ * (C) 2010, Nodejitsu Inc.
+ *
+ */
+ 
+var assert = require('assert'),
+    fs = require('fs'),
+    path = require('path'),
+    mockRequest = require('mock-request'),
+    vows = require('vows'),
+    jitsu = require('../../lib/jitsu'),
+    helper = require('../helpers/mock-helpers');
+
+var mockPrompt2 = helper.mockPrompt2,
+    runJitsuCommand = helper.runJitsuCommand;
+
+vows.describe('jitsu/commands/apps').addBatch({
+  'This test requires jitsu be unauthorized': function () {
+    jitsu.skipAuth = false;
+    assert.isFalse(jitsu.skipAuth);
+  }
+}).addBatch({
+  'env list': runJitsuCommand(
+    mockRequest.mock(helper.mockOptions, helper.mockDefaults)
+      .get('/auth')
+      .get('/apps/mickey/jitsu')
+      .respond({
+        body: {
+          app: { 
+            name: 'application', 
+            state: 'stopped', 
+            env: { foo: 'bar', baz: 'buzz' },
+            subdomain:'application', 
+            scripts: { start: './server.js' }, 
+            snapshots: [{ filename: 'FILENAME' }] 
+          }
+        }
+      }))
+}).addBatch({
+  'env get foo': runJitsuCommand(
+    mockRequest.mock(helper.mockOptions, helper.mockDefaults)
+      .get('/apps/mickey/jitsu')
+      .respond({
+        body: {
+          app: { 
+            name: 'application', 
+            state: 'stopped', 
+            env: { foo: 'bar', baz: 'buzz' },
+            subdomain:'application', 
+            scripts: { start: './server.js' }, 
+            snapshots: [{ filename: 'FILENAME' }] 
+          }
+        }
+      }))
+}).addBatch({
+  'env set test truthy': runJitsuCommand(
+    mockRequest.mock(helper.mockOptions, helper.mockDefaults)
+      .get('/apps/mickey/jitsu')
+      .respond({
+        body: {
+          app: { 
+            name: 'application', 
+            state: 'stopped', 
+            env: { foo: 'bar', baz: 'buzz' },
+            subdomain:'application', 
+            scripts: { start: './server.js' }, 
+            snapshots: [{ filename: 'FILENAME' }] 
+          }
+        }
+      })
+      .put('/apps/mickey/application'))
+}).addBatch({
+  'env set delete test': runJitsuCommand(
+    mockRequest.mock(helper.mockOptions, helper.mockDefaults)
+      .get('/apps/mickey/jitsu')
+      .respond({
+        body: {
+          app: { 
+            name: 'application', 
+            state: 'stopped', 
+            env: { foo: 'bar', baz: 'buzz', test: 'truthy' },
+            subdomain:'application', 
+            scripts: { start: './server.js' }, 
+            snapshots: [{ filename: 'FILENAME' }] 
+          }
+        }
+      })
+      .put('/apps/mickey/application'))
+}).export(module);
