a1238e0a8f799916d41f32155c9f7e9bee9e44a2
Marak Squires <marak.squires@gmail.com>
2011-08-25 00:33:49 -0700

    [api] Completed knockout command, waiting for copydir update.

--- starters/knockout/server.js
+++ starters/knockout/server.js
@@ -1,14 +1,29 @@
-
 var http = require('http');
 
-require('./vendor/nko')({{knockoutcode}}, function(err, res) {
-  if (err) {
-    console.error('Error contacting Node Knockout servers:');
-    console.error(err.stack);
-  }
-});
+//
+// START NODE KNOCKOUT TRACKING CODE
+//
 
+    //
+    // Remark: This code MUST be required ONCE somewhere in your application for,
+    // node knockout tracking to work.
+    //
+    require('./vendor/nko')({{knockoutcode}}, function(err, res) {
+      if (err) {
+        console.error('Error contacting Node Knockout servers:');
+        console.error(err.stack);
+      }
+    });
+
+//
+//  END NODE KNOCKOUT TRACKING CODE
+//
+
+
+//
+// Start up an http server
+//
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>I know Nodejitsu!</h1>');
+  res.end('<h1>I\'m gonna node knock you out.</h1>');
 }).listen(8080);
