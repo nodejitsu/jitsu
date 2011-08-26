d18f7c44c7fc71b8f0c8d14bc90fb2dba90dbc16
Marak Squires <marak.squires@gmail.com>
2011-08-26 16:43:49 -0700

    [fix] helloworld starter listens on port 8000 instead of 80 for ease of use locally. Thanks @erock!

--- starters/helloworld/server.js
+++ starters/helloworld/server.js
@@ -4,4 +4,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('<h1>I know Nodejitsu!</h1>');
-}).listen(80);
+}).listen(8000);
+
+console.log('http server running on port 8000');
