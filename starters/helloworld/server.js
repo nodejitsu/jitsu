4410241fa42bc29208360e9b7600deb1f881dfcf
Charlie McConnell <charlie@charlieistheman.com>
2011-09-20 17:56:27 -0700

    [dist] Removed old helloworld starter.

--- starters/helloworld/server.js
+++
@@ -1,9 +0,0 @@
-
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>I know Nodejitsu!</h1>');
-}).listen(8000);
-
-console.log('http server running on port 8000');
