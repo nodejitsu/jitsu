00ffbf0e43c77c752c80289fdcf62e849ca14fc5
Charlie McConnell <charlie@charlieistheman.com>
2012-02-22 15:59:20 -0800

    [test] Add more env testing (wip).

--- test/commands/env-test.js
+++ test/commands/env-test.js
@@ -46,6 +46,21 @@ vows.describe('jitsu/commands/env').addBatch({
       }, { 'x-powered-by': 'Nodejitsu' })
   })
 }).addBatch({
+  'env list barbaz': shouldNodejitsuOk(function setup() {
+    nock('http://api.mockjitsu.com')
+      .get('/apps/tester/barbaz')
+      .reply(200, {
+        app: { 
+          name: 'barbaz', 
+          state: 'stopped', 
+          env: { ping: 'fizz', pong: 'buzz' },
+          subdomain:'barbaz',
+          scripts: { start: './server.js' }, 
+          snapshots: [{ filename: 'FILENAME' }] 
+        }
+      }, { 'x-powered-by': 'Nodejitsu' })
+  })
+}).addBatch({
   'env get foo': shouldNodejitsuOk(function setup() {
     nock('http://api.mockjitsu.com')
       .get('/apps/tester/jitsu')
@@ -55,6 +70,21 @@ vows.describe('jitsu/commands/env').addBatch({
           state: 'stopped', 
           env: { foo: 'bar', baz: 'buzz' },
           subdomain:'application', 
+          scripts: { start: './server.js' }, 
+          snapshots: [{ filename: 'FILENAME' }] 
+        }
+      }, { 'x-powered-by': 'Nodejitsu' })
+  })
+}).addBatch({
+  'env get barbaz ping': shouldNodejitsuOk(function setup() {
+    nock('http://api.mockjitsu.com')
+      .get('/apps/tester/barbaz')
+      .reply(200, {
+        app: { 
+          name: 'barbaz', 
+          state: 'stopped', 
+          env: { ping: 'fizz', pong: 'buzz' },
+          subdomain: 'barbaz', 
           scripts: { start: './server.js' }, 
           snapshots: [{ filename: 'FILENAME' }] 
         }
@@ -78,6 +108,23 @@ vows.describe('jitsu/commands/env').addBatch({
         .reply(200, '', { 'x-powered-by': 'Nodejitsu' });    
   })
 }).addBatch({
+  'env set barbaz delete test': shouldNodejitsuOk(function setup() {
+    nock('http://api.mockjitsu.com')
+      .get('/apps/tester/barbaz')
+        .reply(200, {
+          app: { 
+            name: 'barbaz', 
+            state: 'stopped', 
+            env: { ping: 'fizz', pong: 'buzz' },
+            subdomain:'barbaz', 
+            scripts: { start: './server.js' }, 
+            snapshots: [{ filename: 'FILENAME' }] 
+          }
+        }, { 'x-powered-by': 'Nodejitsu' })
+      .put('/apps/tester/barbaz', { env: { ping: 'fizz', pong: 'buzz', delete: 'test' } })
+        .reply(200, '', { 'x-powered-by': 'Nodejitsu' });    
+  })
+}).addBatch({
   'env set delete test': shouldNodejitsuOk(function setup() {
     nock('http://api.mockjitsu.com')
       .get('/apps/tester/jitsu')
@@ -94,4 +141,78 @@ vows.describe('jitsu/commands/env').addBatch({
       .put('/apps/tester/application', { env: { foo: 'bar', baz: 'buzz', test: 'truthy', delete: 'test' } })
         .reply(200, '', { 'x-powered-by': 'Nodejitsu' });
   })
+}).addBatch({
+  'env delete delete': shouldNodejitsuOk(function setup () {
+    nock('http://api.mockjitsu.com')
+      .get('/apps/tester/jitsu')
+        .reply(200, {
+          app: { 
+            name: 'application',
+            state: 'stopped', 
+            env: { foo: 'bar', baz: 'buzz', test: 'truthy', delete: 'test' },
+            subdomain:'application', 
+            scripts: { start: './server.js' }, 
+            snapshots: [{ filename: 'FILENAME' }] 
+          }
+        }, { 'x-powered-by': 'Nodejitsu' })
+      .put('/apps/tester/application', { env: { foo: 'bar', baz: 'buzz', test: 'truthy' } })
+        .reply(200, '', { 'x-powered-by': 'Nodejitsu' });
+  })
+}).addBatch({
+  'env delete barbaz delete': shouldNodejitsuOk(function setup () {
+    nock('http://api.mockjitsu.com')
+      .get('/apps/tester/barbaz')
+        .reply(200, {
+          app: { 
+            name: 'barbaz',
+            state: 'stopped', 
+            env: { foo: 'bar', baz: 'buzz', test: 'truthy', delete: 'test' },
+            subdomain:'barbaz', 
+            scripts: { start: './server.js' }, 
+            snapshots: [{ filename: 'FILENAME' }] 
+          }
+        }, { 'x-powered-by': 'Nodejitsu' })
+      .put('/apps/tester/barbaz', { env: { foo: 'bar', baz: 'buzz', test: 'truthy' } })
+        .reply(200, '', { 'x-powered-by': 'Nodejitsu' });
+  })
+}).addBatch({
+  'env clear': shouldNodejitsuOk('The current app should have an empty env',
+    function setup() {
+      jitsu.prompt.override['yesno'] = 'yes';
+      nock('http://api.mockjitsu.com')
+        .get('/apps/tester/jitsu')
+          .reply(200, {
+              app: { 
+                name: 'application',
+                state: 'stopped', 
+                env: { foo: 'bar', baz: 'buzz', test: 'truthy', delete: 'test' },
+                subdomain:'application', 
+                scripts: { start: './server.js' }, 
+                snapshots: [{ filename: 'FILENAME' }] 
+              }
+            }, { 'x-powered-by': 'Nodejitsu' })
+        .put('/apps/tester/application', { env: {} })
+          .reply(200, '', { 'x-powered-by': 'Nodejitsu' });
+    }
+  )
+}).addBatch({
+  'env clear barbaz': shouldNodejitsuOk('The specified app should have an empty env',
+    function setup() {
+      jitsu.prompt.override['yesno'] = 'yes';
+      nock('http://api.mockjitsu.com')
+        .get('/apps/tester/barbaz')
+          .reply(200, {
+              app: { 
+                name: 'barbaz',
+                state: 'stopped', 
+                env: { foo: 'bar', baz: 'buzz', test: 'truthy' },
+                subdomain:'barbaz', 
+                scripts: { start: './server.js' }, 
+                snapshots: [{ filename: 'FILENAME' }] 
+              }
+            }, { 'x-powered-by': 'Nodejitsu' })
+        .put('/apps/tester/barbaz', { env: {} })
+          .reply(200, '', { 'x-powered-by': 'Nodejitsu' });
+    }
+  )
 }).export(module);
