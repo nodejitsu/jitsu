bafec0c5e4e1899f5731756b4ab467c545dbd3c6
Joshua Holbrook <josh.holbrook@gmail.com>
2012-03-01 15:41:57 -0800

    [test] Basic tests for recovering gracefully from malformed jitsu commands

---
+++ test/commands/bad-test.js
@@ -0,0 +1,38 @@
+/*
+ * bad-test.js: Tests for intentionally broken command(s).
+ *
+ * (C) 2010, Nodejitsu Inc.
+ *
+ */
+ 
+var assert = require('assert'),
+    fs = require('fs'),
+    path = require('path'),
+    nock = require('nock'),
+    vows = require('vows'),
+    jitsu = require('../../lib/jitsu'),
+    macros = require('../helpers/macros');
+
+var shouldNodejitsuOk = macros.shouldNodejitsuOk,
+    useAppFixture = macros.useAppFixture;
+
+var mainDirectory = process.cwd();
+
+vows.describe('jitsu/commands/malformed').addBatch({
+  'app list': shouldNodejitsuOk('should exit gracefully')
+}).addBatch({
+  'apps derploy': shouldNodejitsuOk('should exit gracefully')
+}).addBatch({
+  'env lost': shouldNodejitsuOk('should exit gracefully')
+}).addBatch({
+  'install socketio': shouldNodejitsuOk('should exit with an error',
+  function setup() {
+    useAppFixture();
+
+    jitsu.prompt.override.proceed = 'yes';
+  }, function assertion (err, ignore) {
+
+    process.chdir(mainDirectory);
+    assert.ok(err);
+  })
+}).export(module);
