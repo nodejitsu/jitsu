16c18d281c9df333ed7d49ffe6633b39ed6b742b
Joshua Holbrook <josh.holbrook@gmail.com>
2012-03-07 14:45:13 -0800

    [test] logs tests for logged-out status

--- test/commands/logs-test.js
+++ test/commands/logs-test.js
@@ -85,6 +85,77 @@ vows.describe('jitsu/commands/logs').addBatch({
     assert.ok(!err);
   })
 }).addBatch({
+  'logs': shouldNodejitsuOk('should prompt for credentials', function setup() {
+
+    useAppFixture();
+
+    jitsu.config.stores.file.file = path.join(__dirname, '..', 'fixtures', 'logged-out-jitsuconf');
+    jitsu.config.stores.file.loadSync();
+
+    jitsu.prompt.override.username = 'tester';
+    jitsu.prompt.override.password = 'EXAMPLE-PASSWORD';
+    
+    nock('http://api.mockjitsu.com')
+      .post('/logs/tester/example-app', {
+        from: "NOW-1DAY",
+        until: "NOW",
+        rows: 100
+      })
+        .reply(200, {
+          data: [
+            {
+              isjson: true,
+              timestamp: '2012-12-21T02:00:00.000z',
+              inputid: '5656',
+              json: {
+                app: 'example-app',
+                message: 'This is a message on stderr',
+                level: 'info',
+                event: 'drone:stderr',
+                user: 'tester'
+              }
+            },
+            {
+              isjson: true,
+              timestamp: '2012-12-21T01:00:00.000z',
+              inputid: '5656',
+              json: {
+                app: 'example-app',
+                message: 'This is also a message',
+                level: 'info',
+                event: 'drone:stdout',
+                user: 'tester'
+              }
+            },
+            {
+              isjson: true,
+              timestamp: '2012-12-21T00:00:00.000z',
+              inputid: '5656',
+              json: {
+                app: 'example-app',
+                message: 'This is a message',
+                level: 'info',
+                event: 'drone:stdout',
+                user: 'tester'
+              }
+            },
+
+          ],
+          numFound: 3,
+          context: {
+            rows: '50',
+            from: 'NOW-1DAY',
+            until: 'NOW',
+            start: 0,
+            query: 'json.user:tester AND json.app:example-app',
+            order: 'desc'
+          }
+        }, { 'x-powered-by': 'Nodejitsu' })
+  }, function assertion (err) {
+    process.chdir(mainDirectory);
+    assert.ok(!err);
+  })
+}).addBatch({
   'logs app': shouldNodejitsuOk(function setup() {
 
     useAppFixture();
@@ -150,7 +221,144 @@ vows.describe('jitsu/commands/logs').addBatch({
     assert.ok(!err);
   })
 }).addBatch({
+  'logs app': shouldNodejitsuOk('should prompt for credentials', function setup() {
+
+    useAppFixture();
+
+    jitsu.config.stores.file.file = path.join(__dirname, '..', 'fixtures', 'logged-out-jitsuconf');
+    jitsu.config.stores.file.loadSync();
+
+    jitsu.prompt.override.username = 'tester';
+    jitsu.prompt.override.password = 'EXAMPLE-PASSWORD';
+    
+    nock('http://api.mockjitsu.com')
+      .post('/logs/tester/example-app', {
+        from: "NOW-1DAY",
+        until: "NOW",
+        rows: 100
+      })
+        .reply(200, {
+          data: [
+            {
+              isjson: true,
+              timestamp: '2012-12-21T02:00:00.000z',
+              inputid: '5656',
+              json: {
+                app: 'example-app',
+                message: 'This is a message on stderr',
+                level: 'info',
+                event: 'drone:stderr',
+                user: 'tester'
+              }
+            },
+            {
+              isjson: true,
+              timestamp: '2012-12-21T01:00:00.000z',
+              inputid: '5656',
+              json: {
+                app: 'example-app',
+                message: 'This is also a message',
+                level: 'info',
+                event: 'drone:stdout',
+                user: 'tester'
+              }
+            },
+            {
+              isjson: true,
+              timestamp: '2012-12-21T00:00:00.000z',
+              inputid: '5656',
+              json: {
+                app: 'example-app',
+                message: 'This is a message',
+                level: 'info',
+                event: 'drone:stdout',
+                user: 'tester'
+              }
+            },
+
+          ],
+          numFound: 3,
+          context: {
+            rows: '50',
+            from: 'NOW-1DAY',
+            until: 'NOW',
+            start: 0,
+            query: 'json.user:tester AND json.app:example-app',
+            order: 'desc'
+          }
+        }, { 'x-powered-by': 'Nodejitsu' })
+  }, function assertion (err) {
+    process.chdir(mainDirectory);
+    assert.ok(!err);
+  })
+}).addBatch({
   'logs app application': shouldNodejitsuOk(function setup() {
+    
+    nock('http://api.mockjitsu.com')
+      .post('/logs/tester/application', {
+        from: "NOW-1DAY",
+        until: "NOW",
+        rows: 100
+      })
+        .reply(200, {
+          data: [
+            {
+              isjson: true,
+              timestamp: '2012-12-21T02:00:00.000z',
+              inputid: '5656',
+              json: {
+                app: 'application',
+                message: 'This is a message on stderr',
+                level: 'info',
+                event: 'drone:stderr',
+                user: 'tester'
+              }
+            },
+            {
+              isjson: true,
+              timestamp: '2012-12-21T01:00:00.000z',
+              inputid: '5656',
+              json: {
+                app: 'application',
+                message: 'This is also a message',
+                level: 'info',
+                event: 'drone:stdout',
+                user: 'tester'
+              }
+            },
+            {
+              isjson: true,
+              timestamp: '2012-12-21T00:00:00.000z',
+              inputid: '5656',
+              json: {
+                app: 'application',
+                message: 'This is a message',
+                level: 'info',
+                event: 'drone:stdout',
+                user: 'tester'
+              }
+            },
+
+          ],
+          numFound: 3,
+          context: {
+            rows: '50',
+            from: 'NOW-1DAY',
+            until: 'NOW',
+            start: 0,
+            query: 'json.user:tester AND json.app:application',
+            order: 'desc'
+          }
+        }, { 'x-powered-by': 'Nodejitsu' });
+  })
+}).addBatch({
+  'logs app application': shouldNodejitsuOk('should prompt for credentials', function setup() {
+
+    jitsu.config.stores.file.file = path.join(__dirname, '..', 'fixtures', 'logged-out-jitsuconf');
+    jitsu.config.stores.file.loadSync();
+
+    jitsu.prompt.override.username = 'tester';
+    jitsu.prompt.override.password = 'EXAMPLE-PASSWORD';
     
     nock('http://api.mockjitsu.com')
       .post('/logs/tester/application', {
