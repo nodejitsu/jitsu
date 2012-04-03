nodejitsu/jitsu

commit 68ec5cceb61c9414a85a8e99a19012b7c30d9d46

[fix] Don't merge remote `package.json` during deploy
This behavior caused a regression when `scripts.start` was changed
locally. Remote `package.json` would override local setttings and cause
`jitsu.package.validate` to report a problem with missing start script.

This bug could've caused unexpected behavior if `script.start` from
remote `package.json` was still present on local file system -
application would end up starting old `scripts.start`.

lib/jitsu/commands/apps.js
@@ -116,8 +116,7 @@ apps.deploy = function (callback) {
         });
       }
       
-      app = !app ? {} : jitsu.common.formatApp(app, ['snapshots', 'active', 'user', 'state']);
-      var pkg = analyzer.merge({}, local, app);
+      var pkg = analyzer.merge({}, local);
       jitsu.package.validate(pkg, dir, updateSnapshot);  
     });
   }

commit 52b9f53bb96d84d5ad8a238042e29b33e3687a48

[fix] Version comparison regression
Originally caused by: 68ec5cceb61c9414a85a8e99a19012b7c30d9d46

Jitsu ends up comparing the local package.json version to itself. This results
in jitsu thinking the user's app needs a version bump everytime `deploy` is
executed. This commit ensures the remote package.json's version gets merged in.

Another solution would be to possibly remove the scripts object:

    app = jitsu.common.formatApp(app || {}, [
      'snapshots',
      'active',
      'user',
      'state',
      'scripts'
    ]);

Closes #209.

lib/jitsu/commands/apps.js
@@ -139,7 +139,7 @@ apps.deploy = function (callback) {
         }
       }
       else {
-        pkg = analyzer.merge({}, local);
+        pkg = analyzer.merge({}, local, { version: app.version });
         jitsu.package.validate(pkg, dir, updateSnapshot);
       }
     });