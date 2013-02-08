968035b0f196b910ce94bbf6ab19dc2aba19a7bc
Farrin Reid <blakmatrix@gmail.com>
2013-02-07 17:04:36 -0800

    [api][test] token managment needs account password as tokens are not sufficient yet

--- test/commands/tokens-test.js
+++ test/commands/tokens-test.js
@@ -20,36 +20,42 @@ var mainDirectory = process.cwd();
 
 vows.describe('jitsu/commands/tokens').addBatch({
   'tokens list': shouldNodejitsuOk(function setup () {
+    jitsu.prompt.override.password = 'EXAMPLE-PASSWORD';
     nock('http://api.mockjitsu.com')
       .get('/users/tester/tokens')
       .reply(200, {"apiTokens":{}}, { 'x-powered-by': 'Nodejitsu' })
   })
 }).addBatch({
   'tokens list adam': shouldNodejitsuOk( function setup () {
+    jitsu.prompt.override.password = 'EXAMPLE-PASSWORD';
     nock('http://api.mockjitsu.com')
       .get('/users/adam/tokens')
       .reply(200, {"apiTokens":{}}, { 'x-powered-by': 'Nodejitsu' });
   })
 }).addBatch({
   'tokens create': shouldNodejitsuOk( function setup () {
+    jitsu.prompt.override.password = 'EXAMPLE-PASSWORD';
     nock('http://api.mockjitsu.com')
       .post('/users/tester/tokens', {})
       .reply(201, {"operation":"insert"}, { 'x-powered-by': 'Nodejitsu' });
   })
 }).addBatch({
   'tokens create test-token': shouldNodejitsuOk(function setup () {
+    jitsu.prompt.override.password = 'EXAMPLE-PASSWORD';
     nock('http://api.mockjitsu.com')
       .put('/users/tester/tokens/test-token', {})
       .reply(201, {"operation":"insert"}, { 'x-powered-by': 'Nodejitsu' });
   })
 }).addBatch({
   'tokens list': shouldNodejitsuOk( function setup () {
+    jitsu.prompt.override.password = 'EXAMPLE-PASSWORD';
     nock('http://api.mockjitsu.com')
       .get('/users/tester/tokens')
       .reply(200, {"apiTokens":{"test-token":"bbe261ea-aa43-4f6d-a3cf-b971301d459b"}}, { 'x-powered-by': 'Nodejitsu' });
   })
 }).addBatch({
   'tokens destroy test-token': shouldNodejitsuOk(function setup () {
+    jitsu.prompt.override.password = 'EXAMPLE-PASSWORD';
     nock('http://api.mockjitsu.com')
       .delete('/users/tester/tokens/test-token', {})
       .reply(201, {"ok":true,"id":"test-token"}, { 'x-powered-by': 'Nodejitsu' });
@@ -62,18 +68,21 @@ vows.describe('jitsu/commands/tokens').addBatch({
   })
 })*/.addBatch({
   'tokens create adam test-token': shouldNodejitsuOk(function setup () {
+    jitsu.prompt.override.password = 'EXAMPLE-PASSWORD';
     nock('http://api.mockjitsu.com')
       .put('/users/adam/tokens/test-token', {})
       .reply(201, {"operation":"insert"}, { 'x-powered-by': 'Nodejitsu' });
   })
 }).addBatch({
   'tokens list adam': shouldNodejitsuOk( function setup () {
+    jitsu.prompt.override.password = 'EXAMPLE-PASSWORD';
     nock('http://api.mockjitsu.com')
       .get('/users/adam/tokens')
       .reply(200, {"apiTokens":{"test-token":"bbe261ea-aa43-4f6d-a3cf-b971301d459b"}}, { 'x-powered-by': 'Nodejitsu' });
   })
 }).addBatch({
   'tokens destroy adam test-token': shouldNodejitsuOk(function setup () {
+    jitsu.prompt.override.password = 'EXAMPLE-PASSWORD';
     nock('http://api.mockjitsu.com')
       .delete('/users/adam/tokens/test-token', {})
       .reply(201, {"ok":true,"id":"test-token"}, { 'x-powered-by': 'Nodejitsu' });
