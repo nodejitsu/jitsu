c19102f90d9bd415a48cab9d76ad74d0c9cf3ff1
Farrin Reid <blakmatrix@gmail.com>
2013-03-14 19:41:34 -0700

    [test] updated tests, included library tests for npm test

--- test/lib/package-test.js
+++ test/lib/package-test.js
@@ -9,9 +9,13 @@ var assert = require('assert'),
     fs = require('fs'),
     path = require('path'),
     vows = require('vows'),
-    jitsu = require('../../lib/jitsu');
+    jitsu = require('../../lib/jitsu'),
+    macros = require('../helpers/macros');
 
 var mainDirectory = process.cwd();
+
+jitsu.init();
+//jitsu.log.loggers.default.remove(app.log.loggers.default.transports.console); // make silent
 
 function setupPackage (options) {
   options = options || {};
@@ -45,7 +49,7 @@ function isValid(property, value) {
   return false;
 }
 
-var suite = vows.describe('jitsu/package').addBatch({
+var suite = vows.describe('jitsu/lib/package').addBatch({
   'name': {
     'starting with .': {
       topic: setupPackage({ name: '.example-app' }),
@@ -57,8 +61,8 @@ var suite = vows.describe('jitsu/package').addBatch({
     'starting with _': {
       topic: setupPackage({ name: '_example-app' }),
 
-      'should be invalid': function (topic) {
-        assert.ok(!isValid('name', topic.name));
+      'should be valid': function (topic) {
+        assert.ok(isValid('name', topic.name));
       }
     },
     'containing -': {
@@ -75,7 +79,7 @@ var suite = vows.describe('jitsu/package').addBatch({
         assert.ok(!isValid('name', topic.name));
       }
     },
-    'containing .': {
+    'containing interior .': {
       topic: setupPackage({ name: 'example.app' }),
 
       'should be valid': function (topic) {
@@ -101,6 +105,120 @@ var suite = vows.describe('jitsu/package').addBatch({
 
       'should be invalid': function (topic) {
         assert.ok(!isValid('name', topic.name));
+      }
+    },
+    'containing +': {
+      topic: setupPackage({ name: 'example+app' }),
+
+      'should be invalid': function (topic) {
+        assert.ok(!isValid('name', topic.name));
+      }
+    },
+    'containing /': {
+      topic: setupPackage({ name: 'example/app' }),
+
+      'should be invalid': function (topic) {
+        assert.ok(!isValid('name', topic.name));
+      }
+    },
+    'node_modules': {
+      topic: setupPackage({ name: 'node_modules' }),
+
+      'should be invalid': function (topic) {
+        assert.ok(!isValid('name', topic.name));
+      }
+    },
+    'favicon.ico': {
+      topic: setupPackage({ name: 'favicon.ico' }),
+
+      'should be invalid': function (topic) {
+        assert.ok(!isValid('name', topic.name));
+      }
+    },
+    'containing unicode(๛ಠ_ಠ☠☃❤⁂⍨⋙‽)': {
+      topic: setupPackage({ name: '๛ಠ_ಠ☠☃❤⁂⍨⋙‽' }),
+
+      'should be valid': function (topic) {
+        assert.ok(isValid('name', topic.name));
+      }
+    }
+  },
+  'subdomain': {
+    'starting with 9': {
+      topic: setupPackage({ subdomain: '9example' }),
+
+      'should be invalid': function (topic) {
+        assert.ok(!isValid('subdomain', topic.subdomain));
+      }
+    },
+    'starting with _': {
+      topic: setupPackage({ subdomain: '_example' }),
+
+      'should be invalid': function (topic) {
+        assert.ok(!isValid('subdomain', topic.subdomain));
+      }
+    },
+    'containing -': {
+      topic: setupPackage(),
+
+      'should be valid': function (topic) {
+        assert.ok(isValid('subdomain', topic.subdomain));
+      }
+    },
+    'containing spaces': {
+      topic: setupPackage({ subdomain: 'example domain' }),
+
+      'should be invalid': function (topic) {
+        assert.ok(!isValid('subdomain', topic.subdomain));
+      }
+    },
+    'containing .': { //will become invalid soon
+      topic: setupPackage({ subdomain: 'example.domain' }),
+
+      'should be valid': function (topic) {
+        assert.ok(isValid('subdomain', topic.subdomain));
+      }
+    },
+    'containing %': {
+      topic: setupPackage({ subdomain: 'example%domain' }),
+
+      'should be invalid': function (topic) {
+        assert.ok(!isValid('subdomain', topic.subdomain));
+      }
+    },
+    'containing @': {
+      topic: setupPackage({ subdomain: 'example@domain' }),
+
+      'should be invalid': function (topic) {
+        assert.ok(!isValid('subdomain', topic.subdomain));
+      }
+    },
+    'containing :': {
+      topic: setupPackage({ subdomain: 'example:domain' }),
+
+      'should be invalid': function (topic) {
+        assert.ok(!isValid('subdomain', topic.subdomain));
+      }
+    },
+    'end with number': {
+      topic: setupPackage({ subdomain: 'c9' }),
+
+      'should be valid': function (topic) {
+        assert.ok(isValid('subdomain', topic.subdomain));
+      }
+    },
+    'end with -': {
+      topic: setupPackage({ subdomain: 'c9-' }),
+
+      'should be invalid': function (topic) {
+        assert.ok(!isValid('subdomain', topic.subdomain));
+      }
+    },
+    'containing unicode(๛ಠ_ಠ☠☃❤⁂⍨⋙‽)': {
+      topic: setupPackage({ subdomain: '๛ಠ_ಠ☠☃❤⁂⍨⋙‽' }),
+
+      'should be valid': function (topic) {
+        assert.ok(isValid('subdomain', topic.subdomain));
       }
     }
   }
